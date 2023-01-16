e0905ec2ba89d242d1d1b0ab188e65edfbeb808e
Swarup Donepudi <swarup@Swarups-Mac-Studio.local>
2023-01-15 23:27:26 -0800

    updated endpoints to match other languages

--- {{cookiecutter.project_slug}}/main.go
+++ {{cookiecutter.project_slug}}/main.go
@@ -11,15 +11,15 @@ func main() {
 	// GET all routes
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"GET":    []string{"/todos", "todo/:id"},
-			"POST":   []string{"/todo"},
-			"PUT":    []string{"todo/:id"},
-			"DELETE": []string{"todo/:id"},
+			"GET":    []string{"/todos", "todos/:id"},
+			"POST":   []string{"/todos"},
+			"PUT":    []string{"todos/:id"},
+			"DELETE": []string{"todos/:id"},
 		})
 	})
 
 	// GET a single todo
-	r.GET("/todo/:id", func(c *gin.Context) {
+	r.GET("/todos/:id", func(c *gin.Context) {
 		id := c.Param("id")
 		c.JSON(http.StatusOK, gin.H{"id": id})
 	})
@@ -30,20 +30,20 @@ func main() {
 	})
 
 	// POST a new todo
-	r.POST("/todo", func(c *gin.Context) {
-		todo := c.PostForm("todo")
+	r.POST("/todos", func(c *gin.Context) {
+		todo := c.PostForm("todos")
 		c.JSON(http.StatusOK, gin.H{"todo": todo})
 	})
 
 	// PUT an updated todo
-	r.PUT("/todo/:id", func(c *gin.Context) {
+	r.PUT("/todos/:id", func(c *gin.Context) {
 		id := c.Param("id")
 		todo := c.PostForm("todo")
 		c.JSON(http.StatusOK, gin.H{"id": id, "todo": todo})
 	})
 
 	// DELETE a todo
-	r.DELETE("/todo/:id", func(c *gin.Context) {
+	r.DELETE("/todos/:id", func(c *gin.Context) {
 		id := c.Param("id")
 		c.JSON(http.StatusOK, gin.H{"id": id})
 	})
